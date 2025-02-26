146e8cb8b74b051b7b1bd9d6e41b653ab0728d71
Rajat Agrawal <r.agrawal247@gmail.com>
2025-02-26 14:21:24 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,7 +82,7 @@ const BlogIndex = ({ data, location }) => {
                       />
                     </div>
                     <div className="py-2 px-4 flex justify-start items-center sm:w-[90%] w-full">
-                      <h1 className="text-gray-600 font-display text-[22px] font-semibold leading-7">
+                      <h1 className="text-gray-700 font-display text-[22px] font-bold leading-7 tracking-wide">
                         {title}
                       </h1>
                     </div>
